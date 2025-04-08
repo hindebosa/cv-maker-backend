60c1feed122afce9d20614e0004248169c042453
hindebosa <hindebosa@gmail.com>
2025-04-08 15:49:57 +0200

    added jwt strategy

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Get,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -13,5 +23,11 @@ export class UsersController {
       createUserDto.email,
       createUserDto.password,
     );
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Request() req) {
+    return this.usersService.findById(req.user.id);
   }
 }
